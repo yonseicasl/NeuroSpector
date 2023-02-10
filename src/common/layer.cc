3268b2015c0720997395c69647279dc997564d5d
Chanho Park <chanho9451@gmail.com>
2023-02-10 20:30:45 +0900

    Update exception handling and comments in cfg

--- src/common/layer.cc
+++ src/common/layer.cc
@@ -6,6 +6,19 @@ layer_t::layer_t(const std::string name_,
     :name(name_),
      parameters(parameters_),
      stride(stride_) { 
+          // Check layer validity
+          if(parameters.at((unsigned)parameter_t::C) 
+          % parameters.at((unsigned)parameter_t::G) != 0 ||
+          parameters.at((unsigned)parameter_t::K) 
+          % parameters.at((unsigned)parameter_t::G) != 0) {
+               std::cerr << "[Error] in Layer `" << name
+                         << "`, the remainder of input channel (C) (or weight count (K)) / group (G) should be `0`; "
+                         << "C=" << parameters.at((unsigned)parameter_t::C) << ", "
+                         << "K=" << parameters.at((unsigned)parameter_t::K) << ", "
+                         << "G=" << parameters.at((unsigned)parameter_t::G) << " "
+                         << std::endl;
+               exit(0);
+          }
 }
 layer_t::~layer_t() {}
 // Get layer name
